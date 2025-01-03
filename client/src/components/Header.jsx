a9718e60034413a7fb0fafe6abd266f8b71a3b40
Michael Mosquera <michael2000ny@gmail.com>
2025-01-03 17:11:22 -0500

    created site-header class & added css to center logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/images/full-logo-nobuffer.jpg'
 
 const Header = () => {
     return (
-        <header className="header container-fluid d-flex align-items-center justify-content-between">
+        <header className="header site-header container-fluid d-flex align-items-center justify-content-between">
           <div className="logo-container text-center flex-grow-1">
             <img src={logo} alt="New Flow Beauty Salon & Barber Shop Logo" className="header-logo" />
           </div>
