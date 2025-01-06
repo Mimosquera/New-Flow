5da63431e6625916781ce915fc02132276231a49
Michael Mosquera <michael2000ny@gmail.com>
2025-01-06 15:51:51 -0500

    attempted to fix header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,25 @@
-import Navigation from './Navigation.jsx'
+import Navigation from './Navigation.jsx';
 import logo from '../assets/images/full-logo-nobuffer.jpg';
 import React from 'react';
 
 const Header = () => {
-    return (
-        <header className="header site-header container-fluid d-flex align-items-center justify-content-between">
-          <div className="logo-container text-center flex-grow-1">
-            <img src={logo} alt="New Flow Beauty Salon & Barber Shop Logo" className="header-logo" />
-          </div>
-          <Navigation />
-        </header>
-    );
+  return (
+    <header className="site-header container-fluid d-flex align-items-center justify-content-between position-relative">
+      {/* Logo Container */}
+      <div className="logo-container position-absolute top-50 start-50 translate-middle">
+        <img
+          src={logo}
+          alt="New Flow Beauty Salon & Barber Shop Logo"
+          className="header-logo"
+        />
+      </div>
+
+      {/* Navigation Wrapper */}
+      <div className="navigation-wrapper d-flex justify-content-end">
+        <Navigation />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,14 +3,18 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-lg sticky-top">
-      <div className="container-fluid">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item "> <Link className="nav-link light-text" to="/">Home</Link> </li>
-          <li className="nav-item"> <Link className="nav-link light-text" to="/about">About</Link> </li>
-          <li className="nav-item"> <Link className="nav-link light-text" to="/appointments">Appointments</Link> </li>
-        </ul>
-      </div>
+    <nav className="navbar">
+      <ul className="navbar-nav d-flex flex-row">
+        <li className="nav-item">
+          <Link className="nav-link light-text" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link light-text" to="/about">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link light-text" to="/appointments">Appointments</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
