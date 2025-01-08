eaf79ffd7ba98eaff8067c1931886e115d1d3dc6
Michael Mosquera <michael2000ny@gmail.com>
2025-01-08 17:03:39 -0500

    added security to iframe in server.js

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -39,12 +39,11 @@ const GoogleMap = () => {
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3166.492937810876!2d-77.52936438793131!3d37.4726924293863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b10d703a083203%3A0x4064bb814e21c354!2sNew%20Flow%20Beauty%20Salon%20%26%20Barber%20Shop!5e0!3m2!1sen!2sus!4v1736369030373!5m2!1sen!2sus"
         className="google-map-iframe"
-        width="200%"
-        height="400"
-        style={{ border: 0 }}
+        allow="geolocation"
         allowFullScreen=""
         loading="lazy"
         title="Google Maps Location"
+        sandbox="allow-scripts allow-same-origin"
       ></iframe>
     </div>
   );
